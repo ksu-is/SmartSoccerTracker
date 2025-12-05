ff3465978de350f4af28731a8a86ab9e4e2f8042
Jevaughn Dwight Harris <jharr706@students.kennesaw.edu>
2025-12-05 10:58:42 -0500

    Fix player search function and add top leagues

--- ProjectCode.py
+++ ProjectCode.py
@@ -6,6 +6,22 @@ from b24 import BeautifulSoup
 HEADERS = {
   "User-Agent": "Mozilla/5.0"
 }
+TOP_LEAGUES = ["Premier League", "LaLiga", "Serie A"]
 
 def search_player(player_name):
    url = "https://api-football-v1.p.rapidapi.com/v3/players"
+   r = requests.get(url, headers=HEADERS)
+    data = r.json()
+players = data.get("players", [])
+   if not players: 
+     print("No player found.")
+     return none
+
+   for p in players:
+     league = p.get("team", {}).get("tournament", {}).get("name", "")
+     if league in TOP_LEAGUES:
+       return {
+          "id": p.get("name")
+          "name": p.get("name")
+          "team": p.get("team", {}.get("name"),
+          "league": league              
