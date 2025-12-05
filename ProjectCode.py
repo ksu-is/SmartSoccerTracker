326de77eef708511ac9274e3e9338a53d7457dd0
Jevaughn Dwight Harris <jharr706@students.kennesaw.edu>
2025-12-04 22:49:37 -0500

    Update search_player function for API response handling
    Fix issues in search_player function for player retrieval.

--- ProjectCode.py
+++ ProjectCode.py
@@ -11,3 +11,17 @@ def search_player(player_name):
    url = "https://api-football-v1.p.rapidapi.com/v3/players"
    res =  requests.get(url, headers=HEADERS)
    data = res.json()
+
+  players = data.get("players", [])
+   if not players: 
+     print("No player found.")
+     return none
+
+   for p in players:
+     league = p.get("team", {}).get("tournament", {}).get("name", "")
+     if league in TOP_LEAGUES:
+       return {
+          "id": p.get("name")
+          "name": p.get("name")
+          "team": p.get("team", {}.get("name"),
+          "league": league              
